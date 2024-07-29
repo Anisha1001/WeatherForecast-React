11a0e9f4a357aa17b61cf02259ae902ca34eb140
Anisha Chowdhury <anishachowdhury34@gmail.com>
2024-07-29 12:36:50 +0530

    Add new files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import WeatherApp from "./WeatherApp";
+
+function App() {
+  return(
+    <>
+    <WeatherApp/>
+   
+    </>
+  )
+}
+
+export default App
